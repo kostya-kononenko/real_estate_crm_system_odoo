fe621c4d21656e96b284f0ebb36180e51673c21e
kostiantyn <kkononenko3@gmail.com>
2024-01-25 18:06:49 +0200

    created module structure

---
+++ custom-addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Real Estate",
+    "description":"""Real Estate model to show avalible properties""",
+    "version": "1.0",
+    "author": "Kostiantyn Kononenko",
+    "category": "Sales",
+    "depends": ["base"],
+    "data": [],
+    "installable": True,
+    "application": True,
+    'license': 'LGPL-3',
+}
