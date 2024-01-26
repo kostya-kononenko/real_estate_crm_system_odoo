4732708a3b517b40329da73de05b467507dcc06c
kostiantyn <kkononenko3@gmail.com>
2024-01-26 09:37:44 +0200

    created property model

---
+++ custom-addons/real_estate_ads/models/property.py
@@ -0,0 +1,28 @@
+from odoo import fields, models
+
+
+class Property(models.Model):
+    _name = 'estate.property'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
+    _description = 'Estate Properties'
+    _order = 'id desc'
+
+    name = fields.Char(string='Name', required=True)
+    description = fields.Text(string='Description')
+    postcode = fields.Char(string='Postcode')
+    date_availability = fields.Date(string='Available From')
+    expected_price = fields.Float(string='Expected Price', tracking=True)
+    best_offer = fields.Float(string='Best Offer', compute='_compute_best_price')
+    selling_price = fields.Float(string='Selling Price', readonly=True)
+    bedrooms = fields.Integer(string='Bedrooms')
+    living_area = fields.Integer(string='Living Area(sqm)')
+    facades = fields.Integer(string='Facades')
+    garage = fields.Boolean(string='Garage', default=False)
+    garden = fields.Boolean(string='Garden', default=False)
+    garden_area = fields.Integer(string='Garden Area(sqm)')
+    garden_orientation = fields.Selection([('north', 'North'),
+                                           ('south', 'South'),
+                                           ('east', 'East'),
+                                           ('west', 'West')],
+                                          string='Garden Orientation',
+                                          default='north')
