a5823aceef095abd34cba2c8b09e10f4183cc3fe
kostiantyn <kkononenko3@gmail.com>
2024-01-27 19:27:08 +0200

    added a new module Real Estate - Sales Person, create User model and inherit from EstateProperty model

---
+++ custom-addons/real_estate_ads_sales_person/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Real Estate - Sales Person",
+    "description":"""Show real estate properties linked to a sales person""",
+    "version": "1.0",
+    "author": "Kostiantyn Kononenko",
+    "category": "Sales",
+    "depends": ["real_estate_ads", "base"],
+    "data": [
+
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
